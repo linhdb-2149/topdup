873485a960bf17011e2c32d698f8d98ce02effbe
Cong Cuong PHAM <ccuong.ph@gmail.com>
2021-04-13 15:34:58 +0100

    clean code

--- modules/web_app/frontend/src/components/dup-compare/dup-compare.service.js
+++ modules/web_app/frontend/src/components/dup-compare/dup-compare.service.js
@@ -2,7 +2,7 @@ import api from '../../api'
 
 class DupCompareService {
   getSimilarityResults = (compareOption) => {
-    const apiUrl = 'https://alb.topdup.org/api/v1/dup-compare/compare'
+    const apiUrl = '/api/v1/dup-compare/compare'
     return api.post(apiUrl, compareOption)
   }
 }
--- modules/web_app/frontend/src/components/dup-report/dup-report.service.js
+++ modules/web_app/frontend/src/components/dup-report/dup-report.service.js
@@ -1,18 +1,7 @@
 import API from '../../api'
-const simReports = require('./report.json')
 
 class DupReportService {
   getSimilarityRecords = (userId) => {
-    // const foundReports = simReports.map(item => ({
-    //   ...item,
-    //   sim_score: parseFloat(item['sim_score']).toFixed(3)
-    // }))
-    // return Promise.resolve(foundReports)
-    // return API.get(`https://alb.topdup.org/api/v1/similarity-reports?userId=`)
-    //   .then(result => result.data.map(item => ({
-    //     ...item,
-    //     sim_score: parseFloat(item['sim_score']).toFixed(3)
-    //   })))
     return API.get(`/api/v1/similarity-reports?userId=${userId || ''}`)
       .then(result => result.data.map(item => ({
         ...item,
